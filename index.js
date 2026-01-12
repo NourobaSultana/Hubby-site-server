bd6aac9b082b9d32398cda3b97505f61a2f69a26
NourobaSultana <toma.yyy80@gmail.com>
2026-01-12 20:36:21 +0600

    updated group

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = process.env.port || 3000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -28,6 +28,40 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // const hubbyCollection = client.db("hubbyDB").collection("creategroup");
+
+    const database = client.db("createGroupDB");
+    const createGroupCollection = database.collection("creategroup");
+
+    app.get("/creategroup", async (req, res) => {
+      const result = await createGroupCollection.find().toArray();
+      res.send(result);
+    });
+
+    // view data er jonno
+
+    app.get("/creategroup/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await createGroupCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/creategroup", async (req, res) => {
+      const hubbyData = req.body;
+      console.log(hubbyData);
+      const result = await createGroupCollection.insertOne(hubbyData);
+      res.send(result);
+    });
+
+    app.delete("/creategroup/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await createGroupCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
